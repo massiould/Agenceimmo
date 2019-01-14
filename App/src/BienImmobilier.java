9547cc60c2ca2b27120c1bd79718ba0d43868fcf
Karlito44 <kamal.aarab@hotmail.fr>
2019-01-14 17:50:29 +0100

    Modification et ajout de méthodes dans la classe agence

--- App/src/BienImmobilier.java
+++ App/src/BienImmobilier.java
@@ -1,48 +1,47 @@
 import java.util.Date ;
+
 public class BienImmobilier {
+	private String adresse ;
+	private String orientation;
+	private int prix;
+	private Date dateVente ;
+	private Date dateDeDisponibilite;
+	private int numeroBienImmobilier;
+	private Personne vendeur;
+	private Personne acheteur;
 
-		
-		private String adresse ;
-	    private String orientation;
-	    private int prix;
-	    private Date dateVente ;
-	    private Date dateDeDisponibilite;
-	    private int numeroBienImmobilier;
-	    private Personne vendeur;
-	    private Personne acheteur;
-	    
-	    public BienImmobilier (String adresse , String orientation , int prix , Date dateVente ,Date dateDeDisponibilite ,int numeroBienImmobilier, Personne vendeur){
-	     this.adresse = adresse;
-	     this.orientation = orientation ;
-	     this.prix =prix ;
-	     this.dateVente =dateVente;
-	     this.dateDeDisponibilite=dateDeDisponibilite;
-	     this.numeroBienImmobilier=numeroBienImmobilier;
-	     this.vendeur = vendeur;
-	     this.acheteur = null;
-	    }
-	    	
-	   public String getAdresse(){
-		   return this.adresse;
-	   }
-	   public String getOrientation(){
-		   return this.orientation;
-	   }
-	   public int getPrix(){
-		   return this.prix;
-	   }
-	   public Date getDateVente(){
-		   return this.dateVente;
-	   }
-	   public Date getDateDeDisponibilite(){
-		   return this.dateDeDisponibilite;
-	   }
-	   public int getnumeroBienImmobilier(){
-		   return this.numeroBienImmobilier;
-	   }
-	    
-
+	public BienImmobilier (String adresse , String orientation , int prix , Date dateVente ,Date dateDeDisponibilite ,int numeroBienImmobilier, Personne vendeur){
+		this.adresse = adresse;
+		this.orientation = orientation ;
+		this.prix = prix ;
+		this.dateVente = dateVente;
+		this.dateDeDisponibilite = dateDeDisponibilite;
+		this.numeroBienImmobilier = numeroBienImmobilier;
+		this.vendeur = vendeur;
+		this.acheteur = null;
 	}
+
+	public String getAdresse(){
+		return this.adresse;
+	}
+	public String getOrientation(){
+		return this.orientation;
+	}
+	public int getPrix(){
+		return this.prix;
+	}
+	public Date getDateVente(){
+		return this.dateVente;
+	}
+	public Date getDateDeDisponibilite(){
+		return this.dateDeDisponibilite;
+	}
+	public int getnumeroBienImmobilier(){
+		return this.numeroBienImmobilier;
+	}
+
+
+}
 
 
 
--- App/src/RDV.java
+++ App/src/RDV.java
@@ -1,14 +1,13 @@
-import java.util.Date;
-
 /*
  * @author Kamal Aarab
  */
+import java.util.Date;
 
 public class RDV {
 	private Agence agence;
 	private Date dateVente;
 	private Date dateDeDisponibilite;
-	
+
 	public RDV(Agence agence) {
 		this.agence = agence;
 	}
