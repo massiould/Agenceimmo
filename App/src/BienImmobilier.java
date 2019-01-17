bf67c900ffc15bcc576fdcd1cb5660d616139568
Karlito44 <kamal.aarab@hotmail.fr>
2019-01-17 16:07:03 +0100

    Ajout de méthodes sur les classes Agence, Annonce

--- App/src/BienImmobilier.java
+++ App/src/BienImmobilier.java
@@ -7,8 +7,8 @@ public class BienImmobilier {
 	private Date dateVente ;
 	private Date dateDeDisponibilite;
 	private int numeroBienImmobilier;
-	private Personne vendeur;
-	private Personne acheteur;
+	private static Personne vendeur;
+	protected Personne acheteur;
 
 	public BienImmobilier (String adresse , String orientation , int prix , Date dateVente ,Date dateDeDisponibilite ,int numeroBienImmobilier, Personne vendeur){
 		this.adresse = adresse;
@@ -21,26 +21,61 @@ public class BienImmobilier {
 		this.acheteur = null;
 	}
 
+	/**
+	 * 
+	 * @return adresse of BienImmobilier
+	 */
 	public String getAdresse(){
 		return this.adresse;
 	}
+	
+	/**
+	 * 
+	 * @return orientation of BienImmobilier
+	 */
+	
 	public String getOrientation(){
 		return this.orientation;
 	}
+	
+	/**
+	 * 
+	 * @return prix of BienImmobilier
+	 */
 	public int getPrix(){
 		return this.prix;
 	}
+	
+	/**
+	 * 
+	 * @return dateVente of BienImmobilier
+	 */
 	public Date getDateVente(){
 		return this.dateVente;
 	}
+	
+	/**
+	 * 
+	 * @return dateDeDisponibilite of BienImmobilier
+	 */
 	public Date getDateDeDisponibilite(){
 		return this.dateDeDisponibilite;
 	}
+	
+	/**
+	 * 
+	 * @return numeroBienImmobilier of BienImmobilier
+	 */
 	public int getnumeroBienImmobilier(){
 		return this.numeroBienImmobilier;
 	}
 
-
+	/**
+	 * @return the vendeur of BienImmobilier
+	 */
+	public static Personne getVendeur() {
+		return vendeur;
+	}
 }
 
 
--- App/src/Maison.java
+++ App/src/Maison.java
@@ -1,17 +1,75 @@
 import java.util.Date;
 
 public class Maison extends BienImmobilier{
-		private int SurfaceHabitable;
-		private int NombrePiece;
-		private int NombreEtage;
-		private String MoyenDeChauffage ;
+		private int surfaceHabitable;
+		private int nombrePiece;
+		private int nombreEtage;
+		private String moyenDeChauffage;
+		
 	public Maison(String adresse, String orientation, int prix, Date dateVente, Date dateDeDisponibilite,
-			int numeroBienImmobilier) {
-		super(adresse, orientation, prix, dateVente, dateDeDisponibilite, numeroBienImmobilier);
-		this.MoyenDeChauffage=MoyenDeChauffage;
-		this.NombreEtage=NombreEtage;
-		this.NombrePiece=NombrePiece;
+			int numeroBienImmobilier, String moyenDeChauffage, int nombreEtage, int nombrePiece) {
+		super(adresse, orientation, prix, dateVente, dateDeDisponibilite, numeroBienImmobilier, getVendeur());
+		this.setMoyenDeChauffage(moyenDeChauffage);
+		this.setNombreEtage(nombreEtage);
+		this.setNombrePiece(nombrePiece);
 		// TODO Auto-generated constructor stub
 	}
+
+	/**
+	 * @return the surfaceHabitable
+	 */
+	public int getSurfaceHabitable() {
+		return surfaceHabitable;
+	}
+
+	/**
+	 * @param surfaceHabitable the surfaceHabitable to set
+	 */
+	public void setSurfaceHabitable(int surfaceHabitable) {
+		this.surfaceHabitable = surfaceHabitable;
+	}
+
+	/**
+	 * @return the nombrePiece
+	 */
+	public int getNombrePiece() {
+		return nombrePiece;
+	}
+
+	/**
+	 * @param nombrePiece the nombrePiece to set
+	 */
+	public void setNombrePiece(int nombrePiece) {
+		this.nombrePiece = nombrePiece;
+	}
+
+	/**
+	 * @return the nombreEtage
+	 */
+	public int getNombreEtage() {
+		return nombreEtage;
+	}
+
+	/**
+	 * @param nombreEtage the nombreEtage to set
+	 */
+	public void setNombreEtage(int nombreEtage) {
+		this.nombreEtage = nombreEtage;
+	}
+
+	/**
+	 * @return the moyenDeChauffage
+	 */
+	public String getMoyenDeChauffage() {
+		return moyenDeChauffage;
+	}
+
+	/**
+	 * @param moyenDeChauffage the moyenDeChauffage to set
+	 */
+	public void setMoyenDeChauffage(String moyenDeChauffage) {
+		this.moyenDeChauffage = moyenDeChauffage;
+	}
+	
 
 }
